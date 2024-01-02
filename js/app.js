a34109692716e4bc94b749f8770a92d41c9f5db1
Mahfujur Rahman <mahfujurrahmancityu@gmail.com>
2024-01-03 00:23:36 +0600

    single phone card added

--- js/app.js
+++ js/app.js
@@ -2,7 +2,27 @@
 const fetchPhonesApi = (phone) => {
     fetch(`https://openapi.programming-hero.com/api/phones?search=${phone}`)
     .then(res => res.json())
-    .then(data => console.log(data.data));
+    .then(phone => displayPhones(phone.data));
 }
 
-fetchPhonesApi('samsung');
+const displayPhones = (phones) => {
+    // console.log(phones);
+    const phoneContainer = document.getElementById('phone-container');
+    for(const phone of phones) {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card h-100">
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        `;
+        phoneContainer.appendChild(div);
+    }
+}
+
+fetchPhonesApi('iphone')
